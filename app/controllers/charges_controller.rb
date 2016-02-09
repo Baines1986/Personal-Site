0316b7dd9ac0f1bd274336302f4a93330314dc82
Stephen Baines <Stephen@Stephens-MacBook-Air.local>
2016-02-09 07:15:43 +0000

    implemented subscription based product grohack2

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,24 +1,29 @@
 class ChargesController < ApplicationController
 	def create
-	  product = Product.find_by_sku("GROHACK1")
+	  product = Product.find_by_sku("GROHACK2")
 
 	  # Amount in cents
 	  @amount = 500
 
 	  customer = Stripe::Customer.create(
 	    :email => params[:stripeEmail],
-	    :card  => params[:stripeToken]
+	    :card  => params[:stripeToken],
+	    :plan => "GROHACK2"
 	  )
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => product.price_in_cents,
-	    :description => product.full_description,
-	    :currency    => 'usd'
-	  )
+	  # charge = Stripe::Charge.create(
+	  #  :customer    => customer.id,
+	  #  :amount      => product.price_in_cents,
+	  #  :description => product.full_description,
+	  #  :currency    => 'usd'
+	  # )
+
+	  # purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
+      # amount: product.price_in_cents, description: charges.description, currency: charges.price_in_cents,
+	  # customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
 
 	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
-		amount: product.price_in_cents, description: charge.description, currency: charge.currency,
+		amount: product.price_in_cents, description: product.full_description, currency: 'USD',
 	    customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
 
 	  redirect_to purchase
